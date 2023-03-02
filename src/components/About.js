c3cb54d6d96f69b540fced3a81095f490150d2c9
Ali Al Shwely <aadel710@gmail.com>
2023-03-02 12:27:01 -0500

    about.js

--- src/components/About.js
+++ src/components/About.js
@@ -4,7 +4,6 @@ import Button from 'react-bootstrap/Button';
 import Scrollchor from 'react-scrollchor';
 import ArrowScroll from './ArrowScroll.js';
 import { CardColumns, Card } from 'react-bootstrap';
-import { Container, Row, Col } from "react-bootstrap";
 
 const Styles = styled.div`
 
