2613422bff3321987f5280f900aa2797b9eb1615
Ali Al Shwely <aadel710@gmail.com>
2023-03-13 17:27:07 -0400

    new about 2

--- src/components/Spreadsheet.js
+++ src/components/Spreadsheet.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import Papa from 'papaparse';
 import { useTable } from 'react-table';
-import '/src/Spreadsheet.css';
+import './css/Spreadsheet.css';
 
 function SpreadsheetApp() {
   const [data, setData] = useState([]);
