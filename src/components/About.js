7ae1e5cfc3373214fae382899b09ba5c402c20be
Ali Al Shwely <aadel710@gmail.com>
2023-03-13 17:46:02 -0400

    new about 5

--- src/components/About.js
+++ src/components/About.js
@@ -1,109 +1,169 @@
-import React from "react";
-import styled from "styled-components";
-import { CardColumns, Card, Button } from "react-bootstrap";
-import Scrollchor from "react-scrollchor";
-import ArrowScroll from "./ArrowScroll";
+import React from "react"
+import styled from 'styled-components';
+import Button from 'react-bootstrap/Button';
+import Scrollchor from 'react-scrollchor';
+import ArrowScroll from './ArrowScroll.js';
+import { CardColumns, Card, Col, Row } from 'react-bootstrap';
 
 const Styles = styled.div`
-  article {
-    padding-top: 5%;
-    color: grey;
-    width: inherit;
-    height: 100%;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    background-color: #f4f4f4;
+
+article {
+
+  padding-top: 5%;
+  color: grey;
+  width: inherit;
+  height: 150%;
+}
+
+article::before {
+  background: linear-gradient(74deg, rgba(0,0,0,1) 18%, rgba(110,94,117,1) 100%);
+  background-size: cover;
+  position: absolute;
+  top: 0; 
+  left: 0;
+  width: 100%; 
+  height: 100%; 
+  opacity: 1; 
+  content: "";
+}
+
+h1 {
+  color: black;
+
+}
+
+h4 {
+
+  text-align: center;
+  line-height: 150%;;
+
+}
+
+p {
+  color: black;
+  text-align: justify;
+}
+
+.btn {
+
+font-family: "Bungee";
+font-size: 20px;
+
+&:hover {
+
+font-family: "Bungee";
+// text-decoration-line: line-through;
+
+}
+
   }
 
-  h1 {
-    font-size: 3.5rem;
-    margin-bottom: 2rem;
-    color: #212529;
-    text-align: center;
-  }
-
-  h4 {
-    font-size: 1.5rem;
-    line-height: 150%;
-    text-align: center;
-    margin-bottom: 2rem;
-  }
-
-  p {
-    font-size: 1.2rem;
-    color: #212529;
-    text-align: justify;
-  }
-
-  .card {
-    width: 90%;
-    height: 100%;
-    color: white;
-    background-color: rgba(52, 52, 52, 0.8);
-    padding-top: 2rem;
-    padding-bottom: 2rem;
-  }
-
-  .btn {
-    font-family: "Bungee";
-    font-size: 1.5rem;
-
-    &:hover {
-      font-family: "Bungee";
-      text-decoration: underline;
-    }
-  }
 `;
 
+const styles = {
+
+  button: {
+
+    minWidth: '120px',
+    minHeight: '50px',
+    fontSize: '20px',
+    fontFamily: 'Bungee',
+
+  },
+
+  card: {
+
+    width: '90%',
+    height: '100%',
+    color: 'white',
+    backgroundColor: 'rgba(52, 52, 52, .2)',
+    paddingTop: '10px',
+    paddingBottom: '10px',
+
+  },
+
+  body: {
+    height: 'inherit',
+
+  },
+
+  text: {
+    height: 'inherit',
+  },
+
+  columns: {
+
+    display: 'flex',
+    height: 'inherit',
+
+  },
+
+  container: {
+    height: 'inherit',
+  },
+
+  row: {
+
+    height: 'inherit',
+
+  }
+
+};
+
 const StyledButton = styled(Button)`
-  min-width: 120px;
-  min-height: 50px;
-  font-size: 1.5rem;
-  font-family: "Bungee";
+
+}
+
 `;
 
 const StyledScrollchor = styled(Scrollchor)`
-  text-decoration: none;
 
-  &:focus,
-  &:visited,
-  &:link,
-  &:active {
+text-decoration: none;
+
+&:focus, &:visited, &:link, &:active {
     text-decoration: none;
-    color: #fff;
-  }
+    color: #FFF;
+}
 
-  &:hover {
+&:hover {
     color: #111;
-  }
+}
 `;
 
-const About = () => {
-  return (
-    <Styles>
-      <article id="about">
-        <CardColumns>
-          <Card className="mx-auto">
-            <h1>About</h1>
+export const About = () => (
+
+  <Styles>
+    <article id="about">
+      <CardColumns className="mx-auto" style={styles.columns}>
+        <Col>
+          <Card className="mx-auto" style={styles.card}>
+            <h1>About </h1>
+            <br />
+            <br />
             <h4>
-              Toronto-based SAP Software Engineer. Enjoying world wide web
-              since 2000. Focused on innovation, quality and user experience.
+              Toronto-based <keyword>SAP Software Engineer</keyword>. <br />
+              Enjoying world wide web since 2000.<br />
+              Focused on innovation, quality and user experience.
             </h4>
-            <StyledButton>Download CV</StyledButton>
+            <br />
+            <br />
           </Card>
-        </CardColumns>
-        <StyledScrollchor
-          className="mx-auto"
-          to="bio"
-          animate={{ offset: -70, duration: 100 }}
-        >
+        </Col>
+      </CardColumns>
+      <br />
+      <br />
+      <br />
+      <Row styles={styles.row}>
+        <StyledScrollchor className="mx-auto" to="bio" animate={{ offset: -70, duration: 100 }}>
           <ArrowScroll />
         </StyledScrollchor>
-      </article>
-    </Styles>
-  );
-};
-
+      </Row>
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+    </article>
+  </Styles>
+)
 export default About;
